0b9dd36eee9ed003fd851881226dd08e8b4373c9
jmonica9 <jmzmonica99@gmail.com>
2022-07-14 21:54:12 +0800

    More Comfortable: show clocks based on form options

--- src/App.js
+++ src/App.js
@@ -1,38 +1,90 @@
-import React from "react";
+import React , {useState, useEffect} from "react";
 import logo from "./logo.png";
 import "./App.css";
 import Clock from "./Clock.js";
-// import ClockContainer from "./bootstrap";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import WorldClock from "./WorldClock.js"
 
-class App extends React.Component {
-//   const date = new Date();
-// date.toLocaleString('en-GB', { timeZone: 'Asia/Singapore' })
+//so what happen is from App.js we declare 2 new state variable: arrayofTimezones and chosenTimezone
+// then make a form of options, when options selected, that value goes into the chosenTimezone state variable
+// then only when submit Button, the chosenTimezone variable goes into arrayofTimezones and clears chosenTimezone variable
+// this arrayofTimezones is then passed to the WorldClock component as a props
+// inside the WorldClock component, each item inside the arrayofTimezones is displayed as a clock component using map (list and keys)
+// Further explanation: the WorldClock component maps each item inside the array as a prop to the clock component 
+//So for as many items inside the array, there will be as many clock components 
+//each item in the array is mapped out to match the name of Clock props that will be used inside the component 
+
+//additional details: 
+// when using forms, need to use preventdefault 
+// also, use conditional rendering to check if props is received inside worldclock , if not display smth else
+
+class App extends React.Component{
+
+
+constructor(props){
+  super(props)
+  this.state = {
+    chosenTimezone:'',
+    showClock:false,
+    arrayOfTimezones:[]
+    
+  };
+ this.handleSubmit = this.handleSubmit.bind(this);
+   this.handleChange = this.handleChange.bind(this);
+}
+
+  handleChange(event) {
+console.log("selected option")
+    this.setState({chosenTimezone: event.target.value});
+  }
 
 
 
+   handleSubmit(event){
+    //when submit, add chosen time zone into the array
+    this.setState({
+     arrayOfTimezones: this.state.arrayOfTimezones.concat(this.state.chosenTimezone),
+      chosenTimezone: "",
+    });
+    console.log(this.state.arrayOfTimezones);
+    event.preventDefault();
+    this.setState({showClock:true})
+  }
+
+//   async componentWillMount() {
+//     await this.handleChange();
+// }
 
 
-  render() {
-
+ render(){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-           <Container fluid>
-         <Row>
-           <Col><Clock timeZone="America/Los_Angeles" /></Col> 
-          
-          <Col><Clock timeZone="Europe/London" /></Col> 
-          
-           <Col><Clock timeZone="Asia/Singapore"/></Col> 
-                </Row>  
-                    </Container>
-                    </header>
+ 
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your timeZone:
+          <select placeholder="select something!" value={this.state.value} onChange={this.handleChange} >
+           
+            <option value="America/Los_Angeles">America/Los_Angeles</option>
+            <option value="Europe/London">Europe/London</option>
+            <option value="Asia/Singapore">Asia/Singapore</option>
+             <option value="Australia/Melbourne">Australia/Melbourne</option>
+            
+          </select>
+        </label>
+        <input type="submit" value="+" />
+       
+      </form>
+
+{this.state.showClock ? <WorldClock clocks={this.state.arrayOfTimezones} />: <p>Select Timezone First</p>}
+{/* {this.state.showClock ? <Clock timeZone={this.state.timeZone}/> : <p>not showing clock</p>} */}
+ {/* <WorldClock clocks={this.state.arrayOfTimezones} /> */}
+ 
+       </header>
+    
       </div>
     );
   }
-}
+};
 export default App;
--- src/Clock.js
+++ src/Clock.js
@@ -1,6 +1,7 @@
+import { Button } from "bootstrap";
 import React from "react";
+import WorldClock from "./WorldClock";
 //import { render } from "react-dom";
-
 class Clock extends React.Component{
   
   constructor(props){
@@ -8,9 +9,8 @@ class Clock extends React.Component{
     this.state = {date:new Date()};
   }
  
-// a component can have single life cycle
-
   componentDidMount(){
+    console.log(this.props)
     this.timerID=setInterval(()=>this.tick(),1000);
   }
 
@@ -27,7 +27,7 @@ render(){
   return (
     <div>
       <h1>  {`${this.props.timeZone}: ${this.state.date.toLocaleString("en-GB", {
-          timeZone: this.props.timeZone, })}`}</h1>
+          timeZone: this.props.timeZone})}`}</h1>
     </div>
   )
 }
