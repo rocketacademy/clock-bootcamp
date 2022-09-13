2c17c1fcad2f13c8eba57fd3ae200af4f9304443
boxyboxy <eugenengboxiang@gmail.com>
2022-09-13 22:24:22 +0800

    Yet to explore grid system via bootstrap, clock exercise completed with timezones being indicated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Clock.js";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Container>
+            <Row>
+              <Col>
+                <img src={logo} className="App-logo" alt="logo" />
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <p>Hello</p>
+              </Col>
+              <Col>
+                <Clock timeZone="America/Los_Angeles" />
+              </Col>
+              <Col>
+                <Clock timeZone="Europe/London" />
+              </Col>
+              <Col>
+                <Clock timeZone="Asia/Singapore" />
+              </Col>
+              <Col>
+                <Clock timeZone="Asia/Qatar" />
+              </Col>
+            </Row>
+          </Container>
         </header>
       </div>
     );
