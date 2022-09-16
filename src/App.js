9542d6e5816f449ae93c50238ccff2f01e7a70f6
mafiageek <anton_kho@msn.com>
2022-09-16 16:53:59 +0800

    added table style

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Clock />
+          <Clock timezone={'Asia/Singapore'}/>
         </header>
       </div>
     );
--- src/Clock.js
+++ src/Clock.js
@@ -1,6 +1,7 @@
 import React from "react"
+import "./Clock.css";
 
-export default function Clock() {
+export default function Clock(props) {
     const [date, refreshDate] = React.useState(new Date());
 
     function refreshClock() {
@@ -15,7 +16,21 @@ export default function Clock() {
       }, []);
 
     return (
-        <p>{date.toLocaleTimeString()}</p>
-    )
+        <table>
+          <thead>
+            <tr>  
+              <th>Country</th>
+              <th>Clock</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{(props.timezone).split("/")[1]}</td>
+              <td>{date.toLocaleString('en-GB', { timeZone: props.timeZone })}</td>
+            </tr>
+          </tbody>
+        </table>
+
+   )
 }
 
