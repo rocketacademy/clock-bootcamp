3eda1ea4b75be107caa1e8896dc7e2ce2b8e5671
yomansg <br.networkers@gmail.com>
2022-09-17 19:22:33 +0800

    World Clock bootcamp

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React from "react";
 import logo from "./logo.png";
+import Clock from "./Clock.js";
+import { Container, Row, Col } from "react-bootstrap";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function MyLayout() {
+  return (
+    <Container fluid>
+      <Row className="justify-content-md-center">
+        <Col xs lg="2" align="left">
+          Los Angeles:
+        </Col>
+        <Col md="auto">
+          <Clock timeZone="America/Los_Angeles" />
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col xs lg="2" align="left">
+          London:
+        </Col>
+        <Col md="auto">
+          <Clock timeZone="Europe/London" />
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col xs lg="2" align="left">
+          Singapore:
+        </Col>
+        <Col md="auto">
+          <Clock timeZone="Asia/Singapore" />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <img src={logo} className="App-logo" alt="logo" /> <br />
+          <MyLayout />
         </header>
       </div>
     );
---
+++ src/Clock.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    // Initialise component state to contain "date" attribute with current date and time
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        {this.state.date.toLocaleString("en-GB", {
+          timeZone: this.props.timeZone,
+        })}
+      </div>
+    );
+  }
+}
+
+export default Clock;
