4e0fc89bb67948944e332f436244f13a6d3c146d
Nicklye26 <nicklye26@gmail.com>
2022-09-18 23:01:09 +0800

    changed to table format instead of columns and rows clear

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Clock.js";
+// import { Row, Col, Container } from "react-bootstrap";
 
 class App extends React.Component {
   render() {
@@ -8,9 +10,37 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <br />
+          <table>
+            <tr>
+              <th>City</th>
+              <th>Clock</th>
+            </tr>
+            <tr>
+              <td>Ulaanbaatar</td>
+              <td>
+                <Clock timeZone="Asia/Ulaanbaatar" />
+              </td>
+            </tr>
+            <tr>
+              <td>Singapore</td>
+              <td>
+                <Clock timeZone="Asia/Singapore" />
+              </td>
+            </tr>
+            <tr>
+              <td>Moscow</td>
+              <td>
+                <Clock timeZone="Europe/Moscow" />
+              </td>
+            </tr>
+            <tr>
+              <td>London</td>
+              <td>
+                <Clock timeZone="Europe/London" />
+              </td>
+            </tr>
+          </table>
         </header>
       </div>
     );
