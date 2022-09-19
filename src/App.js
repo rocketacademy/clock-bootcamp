c2f551a5aa3f347f27118204535669396acab3bf
sharlearn <liewsharlen@gmail.com>
2022-09-19 21:50:06 +0800

    base with attempt at grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Clock.js"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+class TimeRow extends React.Component {
+  render() {
+    return (
+      <Row>
+        <Col>{this.props.countryName}</Col>
+        <Col><Clock langCode='en-GB' timeZone={this.props.timeZone}/></Col>
+      </Row>
+    )
+  }
+}
 
 class App extends React.Component {
   render() {
@@ -8,12 +23,20 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Container>
+            <Row>
+              <Col>City</Col>
+              <Col>Time</Col>
+            </Row>
+            <TimeRow countryName="Dubai" timeZone="Asia/Dubai"/>
+            <TimeRow countryName='Singapore' timeZone="Asia/Singapore" />
+            <TimeRow countryName="Los Angeles" timeZone="America/Los_Angeles" />
+            <TimeRow countryName="London" timeZone="Europe/London" />
+          </Container>
         </header>
       </div>
     );
+
   }
 }
 
