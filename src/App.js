73e0404c21bafdb96f8da75910f3b57034e95dcf
Cljwen <carmenlaujwn@gmail.com>
2022-09-20 01:04:19 +0800

    World Clock V1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Clock.js";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class App extends React.Component {
   render() {
@@ -8,9 +12,30 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Container className="grid">
+            <Row>
+              <Col>City</Col>
+              <Col>Clock</Col>
+            </Row>
+            <Row>
+              <Col>Singapore</Col>
+              <Col>
+                <Clock timeZone="Asia/Singapore" />
+              </Col>
+            </Row>
+            <Row>
+              <Col>London</Col>
+              <Col>
+                <Clock timeZone="Europe/London" />
+              </Col>
+            </Row>
+            <Row>
+              <Col>Dubai</Col>
+              <Col>
+                <Clock timeZone="Asia/Dubai" />
+              </Col>
+            </Row>
+          </Container>
         </header>
       </div>
     );
