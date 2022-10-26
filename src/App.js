5eb1d681815449455797c374c91de846635ada38
gggaryteo <garyteoprograms@gmail.com>
2022-10-27 01:21:27 +0800

    Attempted Comfortable Clock

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Components/Clock";
+
+/*
+  React Bootstrap Configuration
+*/
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 class App extends React.Component {
   render() {
@@ -8,9 +15,55 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+
+          <Container className="mt-5">
+            <Row>
+              <Col
+                xs={6}
+                style={{ backgroundColor: "grey" }}
+                className="border border-light"
+              >
+                <p>City</p>
+              </Col>
+              <Col
+                style={{ backgroundColor: "grey" }}
+                xs={6}
+                className="border border-light"
+              >
+                <p>Date & Time</p>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col
+                xs={6}
+                className="border border-light"
+              >
+                <p>Singapore</p>
+              </Col>
+              <Col xs={6} className="border border-light">
+                <Clock timeZone="Asia/Singapore" />
+              </Col>
+            </Row>
+
+            <Row>
+              <Col xs={6} className="border border-light">
+                <p>London</p>
+              </Col>
+              <Col xs={6} className="border border-light">
+                <Clock timeZone="Europe/London" />
+              </Col>
+            </Row>
+
+            <Row>
+              <Col xs={6} className="border border-light">
+                <p>Arizona</p>
+              </Col>
+              <Col xs={6} className="border border-light">
+                <Clock timeZone="America/Phoenix" />
+              </Col>
+            </Row>
+          </Container>
         </header>
       </div>
     );
