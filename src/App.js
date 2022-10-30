dbb41f49025234436af61b674ad0fe97a3bdb47e
vponkshe96 <vpponkshe@gmail.com>
2022-10-31 02:45:30 +0800

    upto-base

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
-
+import Clock from "./Clock";
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          Time in Singapore:
+          <Clock timeZone="Asia/Singapore" />
+          Time in Tokyo:
+          <Clock timeZone="Asia/Tokyo" />
+          Time in Brazil:
+          <Clock timeZone="Brazil/East" />
         </header>
       </div>
     );
