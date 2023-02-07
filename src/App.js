2ed07dd13676322c46e9f99c5dc147e5e9c9d55b
peanutyabing <sherry.liuyabing@gmail.com>
2023-02-07 11:38:23 +0800

    Add Clock component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Components/Clock";
 
 class App extends React.Component {
   render() {
@@ -8,9 +9,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Clock />
         </header>
       </div>
     );
---
+++ src/Components/Clock.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: new Date(),
+    };
+  }
+
+  tick = () => {
+    this.setState({
+      date: new Date(),
+    });
+  };
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render() {
+    return <p>{this.state.date.toString()}</p>;
+  }
+}
