6f25f4c63469440a3dc1d4286dc393f81ce91bb5
peanutyabing <sherry.liuyabing@gmail.com>
2023-02-07 14:18:59 +0800

    Format clocks into rows and columns.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import logo from "./logo.png";
 import "./App.css";
 import Clock from "./Components/Clock";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class App extends React.Component {
   render() {
@@ -9,7 +12,22 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Clock />
+          <Container>
+            <Row>
+              <Col>
+                <h1>Time Zone</h1>
+              </Col>
+              <Col>
+                <h1>City</h1>
+              </Col>
+              <Col>
+                <h1>Local Time</h1>
+              </Col>
+            </Row>
+            <Clock timeZone="Asia/Singapore" />
+            <Clock timeZone="America/Toronto" />
+            <Clock timeZone="Pacific/Honolulu" />
+          </Container>
         </header>
       </div>
     );
--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default class Clock extends React.Component {
   constructor(props) {
@@ -24,6 +26,22 @@ export default class Clock extends React.Component {
   }
 
   render() {
-    return <p>{this.state.date.toString()}</p>;
+    return (
+      <Row>
+        <Col>
+          <p>{this.props.timeZone}</p>
+        </Col>
+        <Col>
+          <p>{this.props.timeZone.split("/")[1]}</p>
+        </Col>
+        <Col>
+          <p>
+            {this.state.date.toLocaleString("en-GB", {
+              timeZone: this.props.timeZone,
+            })}
+          </p>
+        </Col>
+      </Row>
+    );
   }
 }
