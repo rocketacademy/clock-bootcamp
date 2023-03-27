e7797486a214ddcd2820d843dab25d07e2e84407
hengmhs <hengmhs@gmail.com>
2023-03-28 07:34:01 +0800

    Add WorldClock component ability to accept an array of timeZones as props to display

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from "react";
 import logo from "./logo.png";
 import "./App.css";
 import Clock from "./Clock.js";
+import WorldClock from "./WorldClock.js";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Clock timeZone="America/Los_Angeles" />
-          <Clock timeZone="Europe/London" />
-          <Clock timeZone="Asia/Singapore" />
+          <WorldClock
+            timeZones={[
+              "America/Los_Angeles",
+              "Europe/London",
+              "Asia/Singapore",
+            ]}
+          />
         </header>
       </div>
     );
--- src/WorldClock.js
+++ src/WorldClock.js
@@ -1,1 +1,20 @@
 import React from "react";
+import Clock from "./Clock.js";
+
+class WorldClock extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const listOfClocks = this.props.timeZones.map((timeZone) => {
+      return (
+        <div key={timeZone}>
+          <Clock timeZone={timeZone} />
+        </div>
+      );
+    });
+    return <div>{listOfClocks}</div>;
+  }
+}
+
+export default WorldClock;
