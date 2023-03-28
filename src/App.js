bb9855ba4b7b94b967a5c57bc983814d48aee0e5
prettykaur <pkaur.dev@gmail.com>
2023-03-29 07:45:56 +0800

    Added JSX Clock component and styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,69 @@
 import React from "react";
-import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Components/Clock";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1 className="header-text">WORLD CLOCK</h1>
         </header>
+
+        <div className="App-body">
+          <div className="body-container">
+            <h2 className="city">SINGAPORE</h2>
+            <div className="time-container">
+              <p>
+                <Clock timeZone="Asia/Singapore" />
+              </p>
+            </div>
+            <hr></hr>
+          </div>
+
+          <div className="body-container">
+            <h2 className="city">SYDNEY</h2>
+            <div className="time-container">
+              <p>
+                <Clock timeZone="Australia/Sydney" />
+              </p>
+            </div>
+            <hr></hr>
+          </div>
+
+          <div className="body-container">
+            <h2 className="city">LONDON</h2>
+            <div className="time-container">
+              <p>
+                <Clock timeZone="Europe/London" />
+              </p>
+            </div>
+            <hr></hr>
+          </div>
+
+          <div className="body-container">
+            <h2 className="city">NEW YORK</h2>
+            <div className="time-container">
+              <p>
+                <Clock timeZone="America/New_York" />
+              </p>
+            </div>
+            <hr></hr>
+          </div>
+
+          <div className="body-container">
+            <h2 className="city">JOHANNESBURG</h2>
+            <div className="time-container">
+              <p>
+                <Clock timeZone="Africa/Johannesburg" />
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
