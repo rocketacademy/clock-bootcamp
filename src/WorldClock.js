bc0edfd688a467668a5e617f72762ce41a8439b0
hengmhs <hengmhs@gmail.com>
2023-03-28 08:20:42 +0800

    Add select form from React docs with flavor theme

--- src/WorldClock.js
+++ src/WorldClock.js
@@ -4,7 +4,27 @@ import Clock from "./Clock.js";
 class WorldClock extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { value: "grapefruit", listOfFlavors: ["lime", "grapefruit"] };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(event) {
+    console.log(event.target.value);
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    //console.log(event.target.value);
+    this.state.listOfFlavors.push(this.state.value);
+    let newListOfFlavors = this.state.listOfFlavors;
+    this.setState({
+      listOfFlavors: newListOfFlavors,
+    });
+    alert("Your favorite flavor is: " + this.state.listOfFlavors.toString());
+    event.preventDefault();
+  }
+
   render() {
     const listOfClocks = this.props.timeZones.map((timeZone) => {
       return (
@@ -13,7 +33,23 @@ class WorldClock extends React.Component {
         </div>
       );
     });
-    return <div>{listOfClocks}</div>;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Pick your favorite flavor:
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <div>{listOfClocks}</div>
+      </div>
+    );
   }
 }
 
