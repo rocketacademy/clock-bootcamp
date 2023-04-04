fbbe708e35887f9709119c37bea292a8ff1cfa2e
root <root@DESKTOP-9SFSVP3>
2023-04-04 23:09:30 +0800

    World Clock

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Clock.js";
 
 class App extends React.Component {
   render() {
@@ -8,9 +9,10 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          {/* Render date value that is stored in state */}
+          <Clock timeZone="America/Los_Angeles" />
+          <Clock timeZone="Europe/London" />
+          <Clock timeZone="Asia/Singapore" />
         </header>
       </div>
     );
