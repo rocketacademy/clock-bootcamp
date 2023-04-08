33ddca93704fce5c2ae81f821086129a1fd8cd3e
coeyyt <coeytky@gmail.com>
2023-04-08 12:39:21 +0800

    new clock

---
+++ src/Clock.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    // Initialise component state to contain "date" attribute with current date and time
+    this.state = { date: new Date() };
+  }
+  componentDidMount() {
+    // Set date value in state every second to current date and time
+    // Save setInterval timer ID in class variable for teardown in another class method
+    this.timerId = setInterval(() => {
+      this.setState({
+        date: new Date(),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    // Teardown setInterval timer with timer ID saved as class variable
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          {`${this.props.timeZone}: ${this.state.date.toLocaleString("en-GB", {
+            timeZone: this.props.timeZone,
+          })}`}
+        </h1>
+      </div>
+    );
+  }
+}
+export default Clock;
