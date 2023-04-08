1d373bf126fe1235f6f11125ba8fb8e8146efb52
cmz1212 <chua.ming.zhou.1212@gmail.com>
2023-04-08 09:59:53 +0800

    Updated all files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Clock.js";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Clock timeZone="America/Los_Angeles" />
+          <Clock timeZone="Europe/London" />
+          <Clock timeZone="Asia/Singapore" />
         </header>
       </div>
     );
