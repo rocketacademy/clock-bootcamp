f2acb1fce0ac0a8c1eab8c06e452b3447252002d
shanquanSQ <thiashanquan93@gmail.com>
2023-09-05 15:10:19 +0800

    normal version

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React from "react";
 import logo from "./logo.png";
 import "./App.css";
 
+import Clock from "./Clock.js";
+
+const locations = ["Asia/Singapore", "Japan", "PST8PDT"];
+
 class App extends React.Component {
+  // Additional functionality
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  // render() is always placed as the last function in a React script
+  // why on line 21 is the date accessed like a method? i.e. this.state.date rather than this.state[date]
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <Clock timeZone={locations[0]} />
+            <Clock timeZone={locations[1]} />
+            <Clock timeZone={locations[2]} />
           </p>
         </header>
       </div>
---
+++ src/Clock.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+//Initialise component state to contain "date" attribute with current date and time
+export default class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () =>
+        this.setState({
+          date: new Date(),
+        }),
+      100
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  //render returns ONLY ONE outer < > html tag
+  render() {
+    // let { timezoneName } = this.props;
+    return (
+      <p>
+        {`Local time in ${this.props.timeZone}`}:
+        <br />
+        {this.state.date.toLocaleString("en-GB", {
+          timeZone: this.props.timeZone,
+        })}
+      </p>
+
+      //alternative to line 30 is simply:
+      // {this.props.timeZone}:
+      // no nested { } and $ -- need nested { } because you want to activate the `${}` found in javascript. if you do not need any strings beforehand.
+
+      // let { timezoneName } = this.props.timeZone;
+      // return (
+      //   <p>
+      //     {this.state.date.toLocaleString("en-GB", {
+      //       timeZone: { timezoneName },
+      //     })}
+      //   </p>
+      // );
+    );
+
+    // return <p>this.state.date.toString()</p> --- will not be javascript function, but just string. need to have curly braces.
+    // return <p>{this.state.date.toString()}</p>;
+  }
+}
