41d4d2ec48cfb60862f6c335616a85971896e2eb
kenleeyx <kenleeyx@gmail.com>
2023-09-07 01:24:40 +0800

    worldclock

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
-import Clock from "./Clock.js";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import WorldClock from "./WorldClock.js";
 
 class App extends React.Component {
   render() {
+    const timeZones = ['Asia/Singapore', 'America/Los_Angeles', 'Europe/London'];
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Container>
-            <Row>
-              <Col>City</Col>
-              <Col>Clock</Col>
-            </Row>
-            <Row>
-              <Col>America/Los_Angeles</Col>
-              <Col><Clock zone="America/Los_Angeles"/></Col>
-            </Row>
-            <Row>
-              <Col>Europe/London</Col>
-              <Col><Clock zone="Europe/London"/></Col>
-            </Row>
-            <Row>
-            <Col>Asia/Singapore</Col>
-            <Col><Clock zone="Asia/Singapore"/></Col>
-            </Row>
-          </Container>
+          <WorldClock clockData = {timeZones}/>
         </header>
       </div>
     );
--- src/WorldClock.js
+++ src/WorldClock.js
@@ -1,33 +1,32 @@
 import React from "react";
+import Clock from "./Clock";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-export default class Clock extends React.Component {
-  constructor(props) {
+export default class WorldClock extends React.Component {
+  constructor(props) { //figure out how to add clockdata prop into here - array of timezone strings
     super(props);
-    this.state = {
-      date: new Date()
-    };
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    })
-  }
-
-  componentDidMount(){
-    this.timerId = setInterval(()=>this.tick(), 1000);
-  }
-
-  componentWillUnmount(){
-    clearInterval(this.timerId);
   }
 
   render() {
-    const {clockData} = this.props
-    return (
-      <div>
-        <p>{this.state.date.toLocaleString('en-GB', {timeZone: zone})}</p>
-      </div>
+    const {clockData} = this.props;
+    const clocks = clockData.map((timeZone)=>
+        <Row key = {timeZone.toString()}>
+            <Col>{timeZone}</Col>
+            <Col><Clock zone={timeZone}/></Col>
+        </Row>
     );
+    return (
+        <div>
+            <Container>
+              <Row>
+                <Col>City</Col>
+                <Col>Clock</Col>
+              </Row>
+              {clocks}
+            </Container>
+        </div>
+      );
   }
 }
