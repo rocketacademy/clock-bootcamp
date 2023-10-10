79ade5ff8b1d394e2c66a7359860072c23e54222
qiannnly <qianlinggg@gmail.com>
2023-10-10 17:43:44 +0800

    added classes to rows and cols

--- src/Components/WorldClock.js
+++ src/Components/WorldClock.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Clock from "./Clock";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./worldClock.css";
 
 class WorldClock extends React.Component {
   render() {
+    const timeZoneOfCountries = this.props.clockData;
+
     return (
-      <div>
-        <Clock timeZone={this.props.clockData} />
-      </div>
+      <Container>
+        {timeZoneOfCountries.map((zone) => (
+          <Row className="col">
+            <Col md={6} className="col">
+              The current time in {zone} is:
+            </Col>
+            <Col md={6} className="col">
+              <Clock timeZone={zone} />
+            </Col>
+          </Row>
+        ))}
+      </Container>
     );
   }
 }
