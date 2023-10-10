e35cab2e88cc3df3de1b87875122f5b0fed44801
qiannnly <qianlinggg@gmail.com>
2023-10-10 16:16:14 +0800

    imported WorldClock and passed in clockData prop array to WorldClock component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import WorldClock from "./WorldClock";
 
 class App extends React.Component {
   render() {
+    const clockData = ["Europe/Zurich", "Canada/Yukon", "Asia/Singapore"];
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <WorldClock clockData={[...clockData]} />
         </header>
       </div>
     );
