a5828777ff403569e4170982e42ff1d79e9a0e05
ianlauyin <ianlauyin@gmail.com>
2023-10-10 22:40:50 +0800

    Base Part Completed

--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// root.render(<App />);
+
+function tick() {
+  root.render(<App />);
+}
+
+setInterval(tick, 1000);
