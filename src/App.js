9eedfee99321d0dd971a966d5ec5825f96b85967
charlesleezhaoyi <charlesleezhaoyi@gmail.com>
2023-10-10 20:53:53 +0800

    Commit #2 - added functions to  set and clear Interval

--- src/App.js
+++ src/App.js
@@ -3,12 +3,33 @@ import logo from "./logo.png";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //initialise component state to contain "date" attribute with current date and time
+    //extends keyword here causes App class to inherit from React.Component
+    //constructor() function sets up the App class with initial arguments called "props"
+    //super keyword causes the App class to update the props argument in React.Component
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({
+        date: new Date(),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>{new Date().toString()}</p>
+          <p>{this.state.date.toString()}</p>
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,16 +4,4 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(<App />);
-
-const tick = () => {
-  const element = (
-    <div>
-      Hello World!
-      <h1>It is {new Date().toLocaleDateString()}.</h1>
-    </div>
-  );
-  root.render(element);
-};
-
-setInterval(tick);
+root.render(<App />);
