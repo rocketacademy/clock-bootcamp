cd81ac8c8c7a9a275323fffd4323c803fc7116ec
ianlauyin <ianlauyin@gmail.com>
2023-10-14 11:08:33 +0800

    UI of multi clock added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Clock";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>{new Date().toString()}</p>
+          {/* Render date value that is stored in state */}
+          <Container fluid>
+            <Row>
+              <Col>City</Col>
+              <Col>Clock</Col>
+            </Row>
+            <Row>
+              <Col>Hong Kong</Col>
+              <Col>
+                <Clock timeZone="Asia/Hong_Kong" />
+              </Col>
+            </Row>
+            <Row>
+              <Col>Japan</Col>
+              <Col>
+                <Clock timeZone="Japan" />
+              </Col>
+            </Row>
+            <Row>
+              <Col>Los Angeles</Col>
+              <Col>
+                <Clock timeZone="America/Los_Angeles" />
+              </Col>
+            </Row>
+          </Container>
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,10 +4,5 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<App />);
 
-function tick() {
-  root.render(<App />);
-}
-
-setInterval(tick, 1000);
+root.render(<App />);
