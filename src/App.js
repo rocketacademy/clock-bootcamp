07f994db9133b4dca71b50c5fdb322d9a7cd75de
charlesleezhaoyi <charlesleezhaoyi@gmail.com>
2023-10-25 20:24:09 +0800

    First commit - Completed during lesson without commit. Will commit upon each change in future

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "../src/Clock";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    //initialise component state to contain "date" attribute with current date and time
-    //extends keyword here causes App class to inherit from React.Component
-    //constructor() function sets up the App class with initial arguments called "props"
-    //super keyword causes the App class to update the props argument in React.Component
-    this.state = { date: new Date() };
-  }
-
-  componentDidMount() {
-    this.timerId = setInterval(() => {
-      this.setState({
-        date: new Date(),
-      });
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerId);
-  }
+  Clock;
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.date.toString()}</p>
+          <Clock timeZone="Europe/Stockholm" />
+          <Clock timeZone="America/Los_Angeles" />
+          <Clock timeZone="Asia/Singapore" />
         </header>
       </div>
     );
